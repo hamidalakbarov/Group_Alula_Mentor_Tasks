33ee1916cd8921ffb727e55aa9c89d7edb824c97
Hamid <hamidalakbarov@gmail.com>
2022-12-25 13:03:30 -0800

    changing OddOrEven file

--- src/OddOrEven.java
+++ src/OddOrEven.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        // Creating scanner for getting inputs from user
+        Scanner input = new Scanner(System.in); // Will get an input from keyboard
+
+        System.out.println("Please enter number");
+        int number = input.nextInt();
+
+        // Solution 1
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number");
+        } else if (number % 2 != 2) {
+            System.out.println(number + " is an odd number");
+        } else {
+            System.out.println(number + " is nether even nor odd");
+        }
+    }
+
 }
 /*
 Numbers -- odd & even
